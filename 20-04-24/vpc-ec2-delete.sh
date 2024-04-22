374b06d29ec221c890963e86818fafc20dc3db07
SargisMinasyan <minasyan.sargis@gmail.com>
2024-04-22 05:46:38 +0000

    fix

--- 20-04-24/vpc-ec2-delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-aws ec2 terminate-instances --instance-ids $insctance_ec2_val
-aws ec2 detach-internet-gateway --internet-gateway-id $internetGateWayId --vpc-id $vpcId_val
-aws ec2 delete-internet-gateway --internet-gateway-id $internetGateWayId
-aws ec2 delete-subnet --subnet-id $subnetId_val
-aws ec2 delete-route-table --route-table-id $routeTable_val
-aws ec2 delete-vpc --vpc-id $vpcId_val
-
-echo sucsses delated 
