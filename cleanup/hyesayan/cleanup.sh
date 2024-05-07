d3ee7cc0bec1fe674043c07d0bcc008c55394951
Hayk <yesayanhaykaz2@gmail.com>
2024-05-07 21:02:14 +0000

    Cleanup script logic updated

--- cleanup/hyesayan/cleanup.sh
+++ cleanup/hyesayan/cleanup.sh
@@ -1,85 +1,44 @@
 #!/bin/bash
 
-# Get instance IDs where permanent is TRUE and terminating that instances:
-instance_ids=$(aws ec2 describe-instances --filters "Name=tag:permanent,Values=true" --query 'Reservations[*].Instances[*].InstanceId' --output text)
-for instance_id in $instance_ids; do
-    echo "Deleting instance with id: $instance_id"
-    aws ec2 terminate-instances --instance-ids $instance_id
-    echo "Instance with id: $instance_id is terminated"
-    echo "Waiting for status terminated"
-    aws ec2 wait instance-terminated --instance-ids $instance_id
-    echo "Status for $instance_id is set to TERMINATED"
-    echo "--------------------------------------------------------"
-done
+# Get resources where permanent is TRUE and delete them:
+resources=(
+    "instance Instances"
+    "vpc Vpcs"
+    "subnet Subnets"
+    "internet-gateway InternetGateways"
+    "route-table RouteTables"
+    "security-group SecurityGroups"
+)
 
-# Get VPC IDs where permanent is TRUE and delete that VPCs:
-vpc_ids=$(aws ec2 describe-vpcs --filters "Name=tag:permanent,Values=true" --query 'Vpcs[0].VpcId' --output text)
-for vpc_id in $vpc_ids; do
-    echo "Deleting VPC with id: $vpc_id"
-    aws ec2 delete-vpc --vpc-id $vpc_id
-    if [ $? -eq 0 ]; then
-    echo "VPC with id: $vpc_id is deleted"
-    echo "--------------------------------------------------------"
-else
-    echo "ERROR: VPC is not deleted"
-    exit
-fi
-done
+for resource in "${resources[@]}"; do
+    resource_type="${resource%% *}"
+    resource_name="${resource#* }"
 
-# Get SUBNET IDs where permanent is TRUE and delete that STUBNETS:
-subnet_ids=$(aws ec2 describe-subnets --filters "Name=tag:permanent,Values=true" --query 'Subnets[1].SubnetId' --output text)
-for subnet_id in $subnet_ids; do
-    echo "Deleting SUBNET with id: $subnet_id"
-    aws ec2 delete-subnet --subnet-id $subnet_id
-    if [ $? -eq 0 ]; then
-    echo "SUBNET with id: $subnet_id is deleted"
-    echo "--------------------------------------------------------"
-else
-    echo "ERROR: SUBNET is not deleted"
-    exit
-fi
-done
+    resource_ids=$(aws ec2 describe-${resource_type}s --filters "Name=tag:permanent,Values=true" --query "${resource_name}[].ResourceId" --output text)
 
-# Get IGW IDs where permanent is TRUE and delete that IGWs:
-igw_ids=$(aws ec2 describe-internet-gateways --filters "Name=tag:permanent,Values=true" --query 'InternetGateways[0].InternetGatewayId' --output text)
-for igw_id in $igw_ids; do
-    echo "Deleting IGW with id: $igw_id"
-    vpc_id=$(aws ec2 describe-internet-gateways --internet-gateway-ids "$igw_id" --query 'InternetGateways[*].Attachments[*].VpcId' --output text)
-    aws ec2 detach-internet-gateway --internet-gateway-id $igw_id --vpc-id $vpc_id
-    aws ec2 delete-internet-gateway --internet-gateway-id $igw_id
-    if [ $? -eq 0 ]; then
-    echo "IGW with id: $igw_id is deleted"
-    echo "--------------------------------------------------------"
-else
-    echo "ERROR: IGW is not deleted"
-    exit
-fi
-done
+    if [ -z "$resource_ids" ]; then
+        echo "No ${resource_type}s found with 'permanent' tag set to 'true'."
+        continue
+    fi
 
-# Get RTB IDs where permanent is TRUE and delete that RTBs:
-rtb_ids=$(aws ec2 describe-route-tables --filters "Name=tag:permanent,Values=true" --query 'RouteTables[0].RouteTableId' --output text)
-for rtb_id in $rtb_ids; do
-    echo "Deleting Route Table with id: $rtb_id"
-    aws ec2 delete-route-table --route-table-id $rtb_id
-    if [ $? -eq 0 ]; then
-    echo "Route table with id: $rtb_id is deleted"
-    echo "--------------------------------------------------------"
-else
-    echo "ERROR: Route table is not deleted"
-    exit
-fi
-done
+    for resource_id in $resource_ids; do
+        if [ "$resource_id" == "None" ]; then
+            echo "Skipping ${resource_type} deletion as the ID is 'None'."
+            continue
+        fi
 
-# Get Secure Group IDs where permanent is TRUE and delete that SGs:
-sec_grs=$(aws ec2 describe-security-groups --filters "Name=tag:permanent,Values=true" --query 'SecurityGroups[0].GroupId' --output text)
-for sec_gr in $sec_grs; do
-    echo "Deleting Security group with id: $sec_gr"
-    aws ec2 delete-security-group --group-id $sec_gr
-    if [ $? -eq 0 ]; then
-    echo "Security group with id: $sec_gr is deleted"
-    echo "--------------------------------------------------------"
-else
-    echo "ERROR: Security group is not deleted"
-    exit
-fi
+        echo "Deleting ${resource_type} with id: $resource_id"
+        if [ "$resource_type" == "instance" ]; then
+            echo "Terminating instance with id: $resource_id"
+            aws ec2 terminate-instances --instance-ids $resource_id
+        else
+            aws ec2 delete-${resource_type} --${resource_type}-id $resource_id
+        fi
+        if [ $? -eq 0 ]; then
+            echo "${resource_type} with id: $resource_id is deleted"
+            echo "--------------------------------------------------------"
+        else
+            echo "ERROR: ${resource_type} with id: $resource_id is not deleted"
+        fi
+    done
 done
