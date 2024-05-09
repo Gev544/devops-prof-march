e3983769d7feb359b7e6934ad1fed55e97873058
Ani777 <ani.grigorian777@gmail.com>
2024-05-09 17:36:20 +0000

    commit

--- cleanup/agrigoryan/cleanup.sh
+++ cleanup/agrigoryan/cleanup.sh
@@ -14,8 +14,28 @@ fi
 aws ec2 wait instance-terminated --instance-ids $INSTANCE_IDS
 
 aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC_ID" --query "Subnets[].SubnetId" --output text | xargs -n 1 aws ec2 delete-subnet --subnet-id
+echo "Step 1: Get the Route Table ID(s) associated with the VPC"
+route_table_ids=$(aws ec2 describe-route-tables \
+    --filters "Name=vpc-id,Values=$VPC_ID" \
+    --query 'RouteTables[*].RouteTableId' \
+    --output text)
 
-aws ec2 describe-internet-gateways --filters "Name=attachment.vpc-id,Values=$VPC_ID" --query "InternetGateways[].InternetGatewayId" --output text | xargs -n 1 aws ec2 detach-internet-gateway --internet-gateway-id
+echo " Step 2: For each Route Table ID, get the list of routes and delete them"
+for rt_id in $route_table_ids; do
+    routes=$(aws ec2 describe-route-tables \
+        --route-table-id $rt_id \
+        --query 'RouteTables[*].Routes[?DestinationCidrBlock != `local`].DestinationCidrBlock' \
+        --output text)
+
+echo    "# Step 3: Delete each route from the route table"
+    for route in $routes; do
+        aws ec2 delete-route \
+            --route-table-id $rt_id \
+            --destination-cidr-block $route
+    done
+done
+
+aws ec2 describe-internet-gateways --filters "Name=attachment.vpc-id,Values=$VPC_ID" --query "InternetGateways[].InternetGatewayId" --output text | xargs -n 1 aws ec2 detach-internet-gateway --internet-gateway-id --vpc-id $VPC_ID
 
 aws ec2 describe-internet-gateways --filters "Name=attachment.vpc-id,Values=$VPC_ID" --query "InternetGateways[].InternetGatewayId" --output text | xargs -n 1 aws ec2 delete-internet-gateway --internet-gateway-id
 
@@ -27,4 +47,4 @@ if [ $? -eq 0 ]; then
     echo "VPC deleted successfully"
 else
     echo "Error: Failed to delete VPC"
-fi
+fi:
