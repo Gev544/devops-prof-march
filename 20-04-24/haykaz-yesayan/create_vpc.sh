1371f2fc1a2d0f5c3e7c616f0969468e9da9900d
Hayk <yesayanhaykaz2@gmail.com>
2024-05-18 12:59:42 +0000

    Changed cleanup logic, merged to 1 script

--- 20-04-24/haykaz-yesayan/create_vpc.sh
+++ 20-04-24/haykaz-yesayan/create_vpc.sh
@@ -2,83 +2,31 @@
 
 set -e
 
-current_date=$(date +"%Y-%m-%d%H:%M:%S")
-
-cleanup() {
-    local error_message="$1"
-    local current_date="$2"
-    echo "Error occurred: $error_message"
-    echo "Starting cleanup!"
-
-    if [ -n "$aws_ec2_id" ]; then
-        aws ec2 terminate-instances --instance-ids "$aws_ec2_id"
-        aws ec2 wait instance-terminated --instance-ids "$aws_ec2_id"
-        echo "EC2 instance $aws_ec2_id terminated"
-    fi
-
-    if [ -n "$security_group_id" ]; then
-        aws ec2 delete-security-group --group-id "$security_group_id"
-        echo "Security group $security_group_id deleted"
-    fi
-
-    if [ -n "$igw_id" ]; then
-        aws ec2 detach-internet-gateway --internet-gateway-id "$igw_id" --vpc-id "$vpc_id"
-        aws ec2 delete-internet-gateway --internet-gateway-id "$igw_id"
-        echo "Internet gateway $igw_id deleted"
-    fi
-
-    if [ -n "$private_subnet_id" ]; then
-        aws ec2 delete-subnet --subnet-id "$private_subnet_id"
-        echo "Private subnet $private_subnet_id deleted"
-    fi
-
-    if [ -n "$public_subnet_id" ]; then
-        aws ec2 delete-subnet --subnet-id "$public_subnet_id"
-        echo "Public subnet $public_subnet_id deleted"
-    fi
-
-    if [ -n "$route_table_id" ]; then
-        aws ec2 delete-route-table --route-table-id "$route_table_id"
-        echo "Route table $route_table_id deleted"
-    fi
-
-    if [ -n "$vpc_id" ]; then
-        aws ec2 delete-vpc --vpc-id "$vpc_id"
-        echo "VPC $vpc_id deleted"
-    fi
-
-    # Delete key pair if created
-    if [ -f "keyPair.pem" ]; then
-        rm keyPair.pem
-        echo "Key pair deleted"
-    fi
-}
-
-trap 'cleanup "$BASH_COMMAND" "$current_date"' ERR
+trap '../../cleanup/hyesayan/cleanup.sh' ERR
 
 
 # Creating VPC
 echo "Starting to create a VPC"
 vpc_id=$(aws ec2 create-vpc --cidr-block "10.0.0.0/16" --region us-east-1 --query 'Vpc.VpcId' --output text)
 echo "VPC is created with ID: $vpc_id"
-aws ec2 create-tags --resources $vpc_id --tags Key='date',Value=$current_date --region us-east-1
+aws ec2 create-tags --resources $vpc_id --tags Key='permanent',Value='true' --region us-east-1
 
 #creating PUBLIC SUBNET
 echo "Starting to create public subnet"
 public_subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block "10.0.1.0/24" --query 'Subnet.SubnetId' --output text)
-aws ec2 create-tags --resources $public_subnet_id --tags Key='date',Value=$current_date --region us-east-1
+aws ec2 create-tags --resources $public_subnet_id --tags Key='permanent',Value='true' --region us-east-1
 echo "Publci subnet is created with ID: $public_subnet_id"
 
 #creating PRIVATE SUBNET
 echo "Starting to create private subnet"
 private_subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block "10.0.2.0/24" --query 'Subnet.SubnetId' --output text)
-aws ec2 create-tags --resources $private_subnet_id --tags Key='date',Value=$current_date --region us-east-1
+aws ec2 create-tags --resources $private_subnet_id --tags Key='permanent',Value='true' --region us-east-1
 echo "Private subnet is created with ID: $private_subnet_id"
 
 #creating IGW
 echo "Starting to create a IGW"
 igw_id=$(aws ec2 create-internet-gateway --query 'InternetGateway.InternetGatewayId' --output text)
-aws ec2 create-tags --resources $igw_id --tags Key='date',Value=$current_date --region us-east-1
+aws ec2 create-tags --resources $igw_id --tags Key='permanent',Value='true' --region us-east-1
 echo "Internet gateway is create with ID: $igw_id"
 #connecting IGW to VPC
 echo "Starting to Attach IGW to VPC"
@@ -88,7 +36,7 @@ echo "Internet gateway is attached to VPC."
 #creating ROUTE TABLE
 echo "Starting to create a route table"
 route_table_id=$(aws ec2 create-route-table --vpc-id $vpc_id --query 'RouteTable.RouteTableId' --output text)
-aws ec2 create-tags --resources $route_table_id --tags Key='date',Value=$current_date
+aws ec2 create-tags --resources $route_table_id --tags Key='permanent',Value='true'
 echo "Route table created with ID: $route_table_id"
 #creating ROUTE
 echo "Starting to create a ROUTE"
@@ -102,7 +50,7 @@ echo "Route table successfully connected to subnet!"
 #creating SECURITY GROUP
 echo "Starting to create a Security group"
 security_group_id=$(aws ec2 create-security-group --vpc-id $vpc_id --group-name "cli_group" --description "Bash CLI group" --output text --query 'GroupId')
-aws ec2 create-tags --resources $security_group_id --tags Key='date',Value=$current_date
+aws ec2 create-tags --resources $security_group_id --tags Key='permanent',Value='true'
 echo "Security group created succesfully!"
 #creating rules for security group
 echo "Starting write inbound rules"
@@ -120,7 +68,6 @@ echo "KeyPair created succesfully!"
 echo "Starting to Lunch an EC2 instance"
 aws_ec2_id=$(aws ec2 run-instances --image-id ami-080e1f13689e07408 --instance-type t2.micro --key-name KeyPairFromShCli --security-group-ids $security_group_id --subnet-id $public_subnet_id --associate-public-ip-address --query 'Instances[0].InstanceId' --output text)
 echo "EC2 lunched succesfully!"
-aws ec2 create-tags --resources $aws_ec2_id --tags Key='date',Value=$current_date
+aws ec2 create-tags --resources $aws_ec2_id --tags Key='permanent',Value='true'
 
-#TESTING CLEANUP FUNCTION
-cleanup "Testing cleanup function" "$current_date"
+echo "$undefined_variable"
--- cleanup/hyesayan/cleanup.sh
+++ cleanup/hyesayan/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Get resources where permanent is TRUE and delete them:
+# Define resources to check and delete
 resources=(
     "instance Instances"
     "vpc Vpcs"
@@ -10,11 +10,82 @@ resources=(
     "security-group SecurityGroups"
 )
 
+delete_vpc_dependencies() {
+    vpc_id=$1
+
+    # Delete subnets
+    subnet_ids=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpc_id" --query "Subnets[*].SubnetId" --output text)
+    for subnet_id in $subnet_ids; do
+        echo "Deleting subnet with id: $subnet_id"
+        aws ec2 delete-subnet --subnet-id $subnet_id
+    done
+
+    # Delete route tables
+    route_table_ids=$(aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$vpc_id" --query "RouteTables[*].RouteTableId" --output text)
+    for route_table_id in $route_table_ids; do
+        echo "Deleting route-table with id: $route_table_id"
+        aws ec2 delete-route-table --route-table-id $route_table_id
+    done
+
+    # Detach and delete internet gateways
+    internet_gateway_ids=$(aws ec2 describe-internet-gateways --filters "Name=attachment.vpc-id,Values=$vpc_id" --query "InternetGateways[*].InternetGatewayId" --output text)
+    for igw_id in $internet_gateway_ids; do
+        echo "Detaching internet-gateway $igw_id from VPC $vpc_id"
+        aws ec2 detach-internet-gateway --internet-gateway-id $igw_id --vpc-id $vpc_id
+        echo "Deleting internet-gateway with id: $igw_id"
+        aws ec2 delete-internet-gateway --internet-gateway-id $igw_id
+    done
+
+    # Delete security groups (except the default one)
+    security_group_ids=$(aws ec2 describe-security-groups --filters "Name=vpc-id,Values=$vpc_id" --query "SecurityGroups[?GroupName!='default'].GroupId" --output text)
+    for sg_id in $security_group_ids; do
+        echo "Deleting security-group with id: $sg_id"
+        aws ec2 delete-security-group --group-id $sg_id
+    done
+}
+
+# Loop through each resource type
 for resource in "${resources[@]}"; do
     resource_type="${resource%% *}"
     resource_name="${resource#* }"
 
-    resource_ids=$(aws ec2 describe-${resource_type}s --filters "Name=tag:permanent,Values=true" --query "${resource_name}[].ResourceId" --output text)
+    # Determine the correct query key based on the resource type
+    case $resource_type in
+        instance)
+            query_key="Instances[*].InstanceId"
+            ;;
+        vpc)
+            query_key="Vpcs[*].VpcId"
+            ;;
+        subnet)
+            query_key="Subnets[*].SubnetId"
+            ;;
+        internet-gateway)
+            query_key="InternetGateways[*].InternetGatewayId"
+            ;;
+        route-table)
+            query_key="RouteTables[*].RouteTableId"
+            ;;
+        security-group)
+            query_key="SecurityGroups[*].GroupId"
+            ;;
+        *)
+            echo "Unknown resource type: $resource_type"
+            continue
+            ;;
+    esac
+
+    # Construct the command to fetch resource IDs
+    command="aws ec2 describe-${resource_type}s --filters \"Name=tag:permanent,Values=true\" --query \"${query_key}\" --output text"
+
+    # Debugging: print the command being run
+    echo "Running: $command"
+
+    # Fetch resource IDs
+    resource_ids=$(eval $command)
+
+    # Debugging: print the fetched IDs
+    echo "Fetched IDs for ${resource_type}: $resource_ids"
 
     if [ -z "$resource_ids" ]; then
         echo "No ${resource_type}s found with 'permanent' tag set to 'true'."
@@ -31,9 +102,25 @@ for resource in "${resources[@]}"; do
         if [ "$resource_type" == "instance" ]; then
             echo "Terminating instance with id: $resource_id"
             aws ec2 terminate-instances --instance-ids $resource_id
+        elif [ "$resource_type" == "vpc" ]; then
+            delete_vpc_dependencies $resource_id
+            aws ec2 delete-vpc --vpc-id $resource_id
+        elif [ "$resource_type" == "internet-gateway" ]; then
+            # Detach the internet gateway from any VPCs before deleting i
+            vpc_id=$(aws ec2 describe-internet-gateways --internet-gateway-ids $resource_id --query "InternetGateways[0].Attachments[0].VpcId" --output text)
+            if [ "$vpc_id" != "None" ]; then
+                echo "Detaching internet-gateway $resource_id from VPC $vpc_id"
+                aws ec2 detach-internet-gateway --internet-gateway-id $resource_id --vpc-id $vpc_id
+            fi
+            aws ec2 delete-internet-gateway --internet-gateway-id $resource_id
+        elif [ "$resource_type" == "security-group" ]; then
+            aws ec2 delete-security-group --group-id $resource_id
         else
-            aws ec2 delete-${resource_type} --${resource_type}-id $resource_id
+            delete_command="aws ec2 delete-${resource_type} --${resource_type}-id $resource_id"
+            echo "Running: $delete_command"
+            $delete_command
         fi
+
         if [ $? -eq 0 ]; then
             echo "${resource_type} with id: $resource_id is deleted"
             echo "--------------------------------------------------------"
@@ -42,3 +129,4 @@ for resource in "${resources[@]}"; do
         fi
     done
 done
+
