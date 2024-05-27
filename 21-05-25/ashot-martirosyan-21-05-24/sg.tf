3ac662ced5673c4123b2c9efdc95fb53028da75e
Ashot <ashotm183@gmail.com>
2024-05-28 00:40:34 +0400

    create ec2 instance using terraform

---
+++ 21-05-25/ashot-martirosyan-21-05-24/sg.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main_security_group"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
